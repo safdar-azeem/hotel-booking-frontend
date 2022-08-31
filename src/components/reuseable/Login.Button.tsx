5fec2f63af32cbd98643afe4808f445b3b1c242c
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-08-31 04:11:58 -0700

    creating login button

---
+++ src/components/reuseable/Login.Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const LoginButton = () => {
+  return (
+    <div>
+    <Button secondary>Sign in</Button>
+    </div>
+
+  )
+}
+
+export default LoginButton
