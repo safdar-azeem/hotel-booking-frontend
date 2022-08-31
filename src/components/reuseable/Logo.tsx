9e3be88584388cce403c4ebe078d8a5059df881b
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-08-31 04:12:33 -0700

    reafctoring in logo

--- src/components/reuseable/Logo.tsx
+++ src/components/reuseable/Logo.tsx
@@ -4,8 +4,9 @@ import { Header } from 'semantic-ui-react'
 const Logo = () => {
   return (
     <div>
-          <Header as='h2' icon='bed' content='Book me' />
+  <Header as='h2' icon='bed' content='Book-Me' />
     </div>
+      
   )
 }
 
