068f88790c2515993791bd5c6eff334c89d8d9c5
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-08-31 04:13:25 -0700

    refactoring in home

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
 import '../../assets/sass/main.scss'
+import LoginButton from '../../components/reuseable/Login.Button'
 import Logo from '../../components/reuseable/Logo'
+import Navigation from '../../components/reuseable/Navigation'
 
 const Home = () => {
 	return (
-		<Container  className='home'>
+		<Container>
+			<section className='header'>
 			<Logo/>
+			<Navigation/>
+			<LoginButton/>
+			</section>
+			
+			
 		</Container>
 	)
 }
