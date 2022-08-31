2649401210a8e548c33e7a1ebee68b8e8d75a1a3
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-08-31 09:13:00 -0700

    adding components in home
    adding navigation
    hero section
    log in btn

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Image } from 'semantic-ui-react'
 import '../../assets/sass/main.scss'
+import HeroHome from '../../components/Hero.Home'
 import LoginButton from '../../components/reuseable/Login.Button'
 import Logo from '../../components/reuseable/Logo'
 import Navigation from '../../components/reuseable/Navigation'
 
 const Home = () => {
+
 	return (
 		<Container>
 			<section className='header'>
@@ -13,7 +15,7 @@ const Home = () => {
 			<Navigation/>
 			<LoginButton/>
 			</section>
-			
+			<HeroHome/>
 			
 		</Container>
 	)
