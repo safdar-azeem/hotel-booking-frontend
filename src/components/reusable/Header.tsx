fd40cb9d32d2306f709528e592b7b1d39a55ff84
safdar-azeem <safdarazeem00@gmail.com>
2022-09-01 15:48:33 +0500

    Add Header for application

---
+++ src/components/reusable/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Button, Container, Grid, Icon } from 'semantic-ui-react'
+import LoginButton from '../../components/reusable/Login-button'
+import Logo from '../../components/reusable/Logo'
+import Navigation from '../../components/reusable/Navigation'
+
+const Header = () => {
+	const [isToggleActive, setIsToggleActvie] = React.useState(false)
+
+	const handleToggleActive = () => setIsToggleActvie(!isToggleActive)
+
+	return (
+		<Container>
+			<Grid columns={3} padded>
+				<Grid.Column mobile={16} tablet={4} computer={4}>
+					<Grid columns={2}>
+						<Grid.Column mobile={8} computer={16}>
+							<Logo />
+						</Grid.Column>
+						<Grid.Column textAlign='right'>
+							<div className='md:hidden block'>
+								<Button icon onClick={handleToggleActive}>
+									<Icon name='bars' />
+								</Button>
+							</div>
+						</Grid.Column>
+					</Grid>
+				</Grid.Column>
+				<Grid.Column
+					mobile={16}
+					tablet={6}
+					computer={6}
+					className={`md:block  ${
+						isToggleActive ? 'block' : 'hidden'
+					}`}>
+					<Navigation />
+				</Grid.Column>
+				<Grid.Column
+					mobile={16}
+					tablet={6}
+					computer={6}
+					className={`md:text-right md:block  ${
+						isToggleActive ? 'block' : 'hidden'
+					}`}>
+					<LoginButton />
+				</Grid.Column>
+			</Grid>
+		</Container>
+	)
+}
+
+export default Header
