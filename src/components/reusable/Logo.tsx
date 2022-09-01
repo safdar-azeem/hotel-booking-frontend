dc25474cb74638fa29a5b1f735025ff075c7a5d3
safdar-azeem <safdarazeem00@gmail.com>
2022-09-01 15:47:47 +0500

    Add logo component

---
+++ src/components/reusable/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react'
+
+const Logo = () => {
+	return (
+		<div>
+			<Header as='h2' icon='bed' content='Book-Me' />
+		</div>
+	)
+}
+
+export default Logo
