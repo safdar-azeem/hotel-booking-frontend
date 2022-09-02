dc59f002b0000123dd65f29e1bf70696b5baca07
safdar-azeem <safdarazeem00@gmail.com>
2022-09-02 16:54:25 +0500

    Refactor : Make logo as Link.

--- src/components/reusable/Logo.tsx
+++ src/components/reusable/Logo.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
-const Logo = () => {
-	return (
-		<div>
-			<Header as='h2' icon='bed' content='Book-Me' />
-		</div>
-	)
-}
+const Logo = () => (
+	<Button
+		basic
+		as={Link}
+		to='/'
+		size='large'
+		icon='bed'
+		content='Book-Me'
+		className='shadow-none'
+	/>
+)
 
 export default Logo
