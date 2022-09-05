8151bd29a95f42955d119e0e071b5a8c3cacc72b
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-05 16:35:39 +0500

    Adding BG color to App

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,7 +1,14 @@
+import HeroSection from '../../components/Hero-section'
 import Header from '../../components/reusable/Header'
 
 const Home = () => {
-	return <Header />
+	return (
+		<div className=' bg-gray-300'>
+			{' '}
+			<Header />
+			<HeroSection />
+		</div>
+	)
 }
 
 export default Home
