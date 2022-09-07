2c5ba06e0b14d2efe3f101cea68a94858ac72dbd
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-07 18:14:52 +0500

    refactoring hotel-card component
    refactoring hotel-card component
    removing import of main.scss file
    replacing some code

--- src/components/reusable/HotelCard.tsx
+++ src/components/reusable/HotelCard.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import {
 	Card,
 	Grid,
+	Header,
 	HeaderContent,
 	HeaderSubheader,
 	Icon,
 	Image,
 } from 'semantic-ui-react'
-import '../../assets/sass/main.scss'
 
 const Hotel = () => {
+	const navigate = useNavigate()
 	const src =
 		'https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 	return (
@@ -18,52 +20,50 @@ const Hotel = () => {
 			computer={5}
 			tablet={8}
 			mobile={16}>
-			<HeaderContent className='hotel-card'>
-				<Image
-					src={src}
-					className=' h-56 w-full rounded-md'
-				/>
-				<HeaderSubheader className='card-text-section'>
-					<HeaderSubheader className='text-gray-500 mt-4'>
-						<Icon name='map marker alternate' />
-						<span className='text-[12px]'>Lahore</span>
-					</HeaderSubheader>
-					<HeaderContent className='flex justify-between'>
-						<HeaderSubheader
-							className='font-extrabold'
-							as={'h1'}>
-							Serena Hotel
-						</HeaderSubheader>
-						<HeaderSubheader
-							as={'h1'}
-							className='flex'>
+			<Card
+				onClick={() => {
+					navigate('/hotel')
+				}}>
+				<Card.Content>
+					<Image
+						className='rounded-md'
+						src={src}
+						wrapped
+						ui={false}
+					/>
+					<Card.Meta className='mt-4'>
+						<span>
+							<Icon name='map marker alternate' /> lahore
+						</span>
+					</Card.Meta>
+
+					<Card.Header className='flex items-center justify-between mt-4'>
+						<Card.Content>Matthew</Card.Content>
+						<Card.Content className='text-base items-center '>
 							<Icon
-								className=' text-yellow-400'
 								name='star'
+								color='yellow'
 							/>
-							<HeaderSubheader className=' text-gray-500 font-normal'>
-								4.5
-							</HeaderSubheader>
-						</HeaderSubheader>
-					</HeaderContent>
-					<HeaderContent className='flex justify-between'>
-						<HeaderContent className=' font-extrabold'>
-							$100/{' '}
-							<HeaderContent
-								as={'span'}
-								className='text-gray-500 font-normal'>
-								night
-							</HeaderContent>
-						</HeaderContent>
-						<HeaderContent>
-							<Icon
-								name='save'
-								disabled
-							/>
-						</HeaderContent>
-					</HeaderContent>
-				</HeaderSubheader>
-			</HeaderContent>
+							<span>4.5</span>
+						</Card.Content>
+					</Card.Header>
+					<Card.Content className='flex justify-between items-center'>
+						<Card.Content className='flex items-center mt-4'>
+							<Card.Content className=' text-xl font-bold'>
+								100$/
+							</Card.Content>
+							<Card.Meta>
+								<span>Night</span>
+							</Card.Meta>
+						</Card.Content>
+						<Icon
+							name='bookmark outline'
+							size='large'
+							className=' outline-none'
+						/>
+					</Card.Content>
+				</Card.Content>
+			</Card>
 		</Grid.Column>
 	)
 }
