b9e344d9cf1681a4f2e84472d8de9103fefe4252
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-07 18:26:59 +0500

    Refactoring login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Button, Form, Header, Image } from 'semantic-ui-react'
+import '../../assets/sass/main.scss'
 
 const Login = () => {
+	const src =
+		'https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
 	return (
-		<section>
-			<h1>Login</h1>
-		</section>
+		<Header className='w-full relative'>
+			<Image
+				src={src}
+				size='huge'
+				className='w-full h-screen'
+			/>
+			<Header className='form-header p-10 '>
+				<Header className='sign-up-text'>Sign Up Form</Header>
+				<Form>
+					<Form.Field>
+						<label>Enter Your Email</label>
+						<input placeholder='Enter Your Email' />
+					</Form.Field>
+					<Form.Field>
+						<label>Enter Your Password</label>
+						<input placeholder='Enter Your Password' />
+					</Form.Field>
+					<Button
+						secondary
+						type='submit'
+						className='w-full mt-5'>
+						Submit
+					</Button>
+				</Form>
+			</Header>
+		</Header>
 	)
 }
 
