fadfa7e4eb1391c1d50e012286c5f365b428988f
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-07 12:00:20 +0500

    adding links in buttons
    adding links in login signup buttons

--- src/components/reusable/Login-button.tsx
+++ src/components/reusable/Login-button.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom'
+import { Routes } from '../../types/routes.type'
 
 const LoginButton = () => {
 	return (
 		<div>
-			<Button secondary>Sign in</Button>
+			<Button
+				secondary
+				as={NavLink}
+				to={Routes.Login}>
+				Sign in
+			</Button>
+			<Button
+				secondary
+				as={NavLink}
+				to={Routes.SignUp}>
+				Sign Up
+			</Button>
 		</div>
 	)
 }
