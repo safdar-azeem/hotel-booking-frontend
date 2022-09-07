662a440436f15a75b2e498261c70ad4f8e9a425e
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-07 11:59:14 +0500

    Adding sign-Up Page

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import {
+	Button,
+	Checkbox,
+	Form,
+	Header,
+	Image,
+	ImageGroup,
+} from 'semantic-ui-react'
+import '../../assets/sass/main.scss'
 
 const SignUp = () => {
+	const src =
+		'https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
 	return (
-		<section>
-			<h1>sign-up</h1>
-		</section>
+		<Header className='w-full relative'>
+			<Image
+				src={src}
+				size='huge'
+				className='w-full h-screen'
+			/>
+			<Header className='form-header p-10'>
+				<Header className='sign-up-text'>Sign Up Form</Header>
+				<Form>
+					<Form.Field>
+						<label>Enter Your Name</label>
+						<input placeholder='Enter Your Name' />
+					</Form.Field>
+					<Form.Field>
+						<label>Enter Your Email</label>
+						<input placeholder='Enter Your Email' />
+					</Form.Field>
+					<Form.Field>
+						<label>Enter Your Password</label>
+						<input placeholder='Enter Your Password' />
+					</Form.Field>
+					<Button
+						secondary
+						type='submit'
+						className='w-full mt-5'>
+						Submit
+					</Button>
+				</Form>
+			</Header>
+		</Header>
 	)
 }
 
