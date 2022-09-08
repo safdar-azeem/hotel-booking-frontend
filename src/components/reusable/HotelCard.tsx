9f58960a04cb7c696aade21e32ad8681ee232445
safdar-azeem <safdarazeem00@gmail.com>
2022-09-08 14:20:20 +0500

    Refactor hotel card

--- src/components/reusable/HotelCard.tsx
+++ src/components/reusable/HotelCard.tsx
@@ -1,17 +1,8 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import {
-	Card,
-	Grid,
-	Header,
-	HeaderContent,
-	HeaderSubheader,
-	Icon,
-	Image,
-} from 'semantic-ui-react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Card, Grid, Header, Icon, Image } from 'semantic-ui-react'
+import { Routes } from '../../types/routes.type'
 
 const Hotel = () => {
-	const navigate = useNavigate()
 	const src =
 		'https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 	return (
@@ -20,48 +11,55 @@ const Hotel = () => {
 			computer={5}
 			tablet={8}
 			mobile={16}>
-			<Card
-				onClick={() => {
-					navigate('/hotel')
-				}}>
+			<Card className='border-0 outline-none shadow-lg rounded-lg md:w-full mx-auto'>
 				<Card.Content>
-					<Image
-						className='rounded-md'
-						src={src}
-						wrapped
-						ui={false}
-					/>
-					<Card.Meta className='mt-4'>
-						<span>
-							<Icon name='map marker alternate' /> lahore
-						</span>
+					<Link to={Routes.Hotel}>
+						<Image
+							src={src}
+							wrapped
+							ui={false}
+						/>
+					</Link>
+					<Card.Meta className='my-3'>
+						<Icon name='map marker alternate' />
+						<span className='date'>Lahore</span>
 					</Card.Meta>
-
-					<Card.Header className='flex items-center justify-between mt-4'>
-						<Card.Content>Matthew</Card.Content>
-						<Card.Content className='text-base items-center '>
+					<Card.Content
+						extra
+						className='flex  justify-between mb-0 items-center'>
+						<Link to={Routes.Hotel}>
+							<Header
+								as='h3'
+								className='mb-0'>
+								Serena Hotel
+							</Header>
+						</Link>
+						<div className='flex items-center'>
 							<Icon
 								name='star'
 								color='yellow'
+								className='mb-2'
 							/>
-							<span>4.5</span>
-						</Card.Content>
-					</Card.Header>
-					<Card.Content className='flex justify-between items-center'>
-						<Card.Content className='flex items-center mt-4'>
-							<Card.Content className=' text-xl font-bold'>
-								100$/
-							</Card.Content>
-							<Card.Meta>
-								<span>Night</span>
-							</Card.Meta>
-						</Card.Content>
-						<Icon
-							name='bookmark outline'
-							size='large'
-							className=' outline-none'
-						/>
+							<Header
+								size='tiny'
+								className='flex-grow my-0'>
+								4.5
+							</Header>
+						</div>
 					</Card.Content>
+				</Card.Content>
+				<Card.Content
+					extra
+					className='flex justify-between pb-0'>
+					<Header
+						size='tiny'
+						className='flex-grow'>
+						$100<span className='text-gray-400'>/Night</span>
+					</Header>
+					<Icon
+						name='bookmark outline'
+						className='ml-0 outline-none cursor-pointer'
+					/>
 				</Card.Content>
 			</Card>
 		</Grid.Column>
