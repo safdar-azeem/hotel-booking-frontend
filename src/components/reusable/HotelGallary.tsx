971350ca7e863521ec20b332075c3ac1d623093d
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-10 09:51:24 +0500

    refactoring in hotelGallery
    *removing whitespace
    *adding loop on hotelCardJson

--- src/components/reusable/HotelGallary.tsx
+++ src/components/reusable/HotelGallary.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Container, Grid } from 'semantic-ui-react'
 import Hotel from './HotelCard'
-
+import hotelCardJson from '../../json/hotel-card-json.json'
+import { IHotelCard } from '../../types/routes.type'
 const HotelGellary = () => {
 	return (
 		<Grid>
 			<Grid.Row className='gap-y-10'>
-				{[...Array(10)].map((_, i) => (
-					<Hotel />
+				{hotelCardJson.map((hotelCard) => (
+					<Hotel hotelCard={hotelCard} />
 				))}
 			</Grid.Row>
 		</Grid>
