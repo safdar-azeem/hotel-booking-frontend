e7919a3a91e9c8990597b0665a74dbeed6ea4b1f
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-10 07:21:24 +0500

    Adding SRC of image in utils
    adding src of image of login and signup in utilis for the clean code

---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export const loginImgSrc =
+	'https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
