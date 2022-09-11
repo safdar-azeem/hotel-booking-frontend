219aa1acb1e8023c04ddf3fb3857493b327df490
safdar-azeem <safdarazeem00@gmail.com>
2022-09-11 19:39:36 +0500

    include modal video

--- src/components/Hero-section.tsx
+++ src/components/Hero-section.tsx
@@ -7,6 +7,7 @@ import {
 	Icon,
 	Image,
 	Input,
+	Modal,
 } from 'semantic-ui-react'
 import '../assets/sass/main.scss'
 import 'react-date-range/dist/styles.css' // main css file
@@ -16,12 +17,14 @@ import DatePicker from './reusable/Date-Picker'
 import SearchOptions from './reusable/Search-Options'
 import SearchComponent from './reusable/Search-Component'
 import { Routes } from '../types/routes.type'
+import ModalVideo from './reusable/Modal-video'
 
 const HeroSection = () => {
 	const src =
 		'https://images.unsplash.com/photo-1615874959474-d609969a20ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
 
 	const [distination, setDistination] = useState('')
+	const [openVideoModal, setOpenVideoModal] = React.useState(false)
 
 	const [openDate, setOpenDate] = useState(false)
 	const [date, setDate] = useState([
@@ -38,31 +41,41 @@ const HeroSection = () => {
 	}
 
 	return (
-		<Container className='hero-section'>
-			<Grid className='p-2 pb-0'>
+		<Container className='hero-section shadow-2xl'>
+			<Grid>
 				<Grid.Column
 					mobile={16}
 					tablet={16}
 					computer={8}>
-					<div className='mr-80'>
+					<div className='md:pr-6'>
 						<Header
-							className='mb-10'
+							className='mb-7 text-[40px]'
 							as={'h1'}>
 							Find your perfect place to stay
 						</Header>
 						<Header
-							className='text-gray-400 text-base font-thin hero-text mb-10'
+							className='text-gray-500 text-2xl font-thin hero-text mb-10'
 							as={'p'}>
-							Lorem ipsum dolor.heloye to your by ipsum dolorsit
-							with famouse link from will to lorums
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Suscipit error veritatis hic mollitia culpa
+							vel ipsa doloremque eligendi. Magnam voluptatem
 						</Header>
-						<Button
-							onClick={() => {
-								navigate(Routes.DemoVideo)
-							}}
-							color='violet'>
-							<Icon name='play circle' /> Watch Video
-						</Button>
+						<Modal
+							onClose={() => setOpenVideoModal(false)}
+							onOpen={() => setOpenVideoModal(true)}
+							open={openVideoModal}
+							trigger={
+								<Button
+									color='violet'
+									size='big'>
+									<Icon name='play circle' /> Watch Video
+								</Button>
+							}>
+							<ModalVideo
+								setOpenVideoModal={setOpenVideoModal}
+								videoUrl='https://www.youtube.com/watch?v=1fDn7a4TvwI'
+							/>
+						</Modal>
 					</div>
 				</Grid.Column>
 				<Grid.Column
