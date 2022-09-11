db621103ff6d138bfd526f008f49714962b65e71
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-11 10:00:04 +0500

    refactoring in hotelGallery
    *changing name from IHotelCard to IHotel
    *Changing path from routes.type tp hotel-card-type

--- src/components/reusable/HotelGallary.tsx
+++ src/components/reusable/HotelGallary.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Container, Grid } from 'semantic-ui-react'
 import Hotel from './HotelCard'
 import hotelCardJson from '../../json/hotel-card-json.json'
-import { IHotelCard } from '../../types/routes.type'
+import { IHotel } from '../../types/hotel-card-type'
 const HotelGellary = () => {
 	return (
 		<Grid>
 			<Grid.Row className='gap-y-10'>
-				{hotelCardJson.map((hotelCard) => (
-					<Hotel hotelCard={hotelCard} />
+				{hotelCardJson.map((value: IHotel) => (
+					<Hotel hotel={value} />
 				))}
 			</Grid.Row>
 		</Grid>
