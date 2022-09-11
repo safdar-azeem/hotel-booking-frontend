c251fe09c2b2f9120be98c29c167b68f79185f4c
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-11 10:01:33 +0500

    refactoring in HotelCard

--- src/components/reusable/HotelCard.tsx
+++ src/components/reusable/HotelCard.tsx
@@ -1,67 +1,74 @@
 import { Link } from 'react-router-dom'
 import { Card, Grid, Header, Icon, Image } from 'semantic-ui-react'
 import { Routes } from '../../types/routes.type'
-import { IHotelCard } from '../../types/routes.type'
+import { IHotel } from '../../types/hotel-card-type'
 
-const Hotel = ({ hotelCard }) => {
+interface IHotelProps {
+	hotel: IHotel
+}
+
+const Hotel = ({ hotel }: IHotelProps) => {
 	return (
 		<Grid.Column
 			largeScreen={4}
 			computer={5}
 			tablet={8}
 			mobile={16}>
-			<Card className='border-0 outline-none shadow-lg rounded-lg md:w-full mx-auto'>
-				<Card.Content>
-					<Link to={Routes.Hotel}>
-						<Image
-							src={hotelCard.imgSrc}
-							wrapped
-							ui={false}
-						/>
-					</Link>
-					<Card.Meta className='my-3'>
-						<Icon name='map marker alternate' />
-						<span className='date'>{hotelCard.location}</span>
-					</Card.Meta>
+			{hotel && (
+				<Card className='border-0 outline-none shadow-lg rounded-lg md:w-full mx-auto'>
+					<Card.Content>
+						<Link to={Routes.Hotel}>
+							<Image
+								className='rounded-md'
+								src={hotel.imgSrc}
+								wrapped
+								ui={false}
+							/>
+						</Link>
+						<Card.Meta className='my-3'>
+							<Icon name='map marker alternate' />
+							<span className='location'>{hotel.location}</span>
+						</Card.Meta>
+						<Card.Content
+							extra
+							className='flex  justify-between mb-0 items-center'>
+							<Link to={Routes.Hotel}>
+								<Header
+									as='h3'
+									className='mb-0'>
+									{hotel.hotelTitle}
+								</Header>
+							</Link>
+							<div className='flex items-center'>
+								<Icon
+									name='star'
+									color='yellow'
+									className='mb-2'
+								/>
+								<Header
+									size='tiny'
+									className='flex-grow my-0'>
+									{hotel.rating}
+								</Header>
+							</div>
+						</Card.Content>
+					</Card.Content>
 					<Card.Content
 						extra
-						className='flex  justify-between mb-0 items-center'>
-						<Link to={Routes.Hotel}>
-							<Header
-								as='h3'
-								className='mb-0'>
-								{hotelCard.hotelTitle}
-							</Header>
-						</Link>
-						<div className='flex items-center'>
-							<Icon
-								name='star'
-								color='yellow'
-								className='mb-2'
-							/>
-							<Header
-								size='tiny'
-								className='flex-grow my-0'>
-								{hotelCard.rating}
-							</Header>
-						</div>
+						className='flex justify-between pb-0'>
+						<Header
+							size='tiny'
+							className='flex-grow'>
+							{hotel.price}
+							<span className='text-gray-400'>/Night</span>
+						</Header>
+						<Icon
+							name='bookmark outline'
+							className='ml-0 outline-none cursor-pointer'
+						/>
 					</Card.Content>
-				</Card.Content>
-				<Card.Content
-					extra
-					className='flex justify-between pb-0'>
-					<Header
-						size='tiny'
-						className='flex-grow'>
-						{hotelCard.price}
-						<span className='text-gray-400'>/Night</span>
-					</Header>
-					<Icon
-						name='bookmark outline'
-						className='ml-0 outline-none cursor-pointer'
-					/>
-				</Card.Content>
-			</Card>
+				</Card>
+			)}
 		</Grid.Column>
 	)
 }
