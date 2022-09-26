87bbb11ee54ba71d600d59b877ee73a3ac36196f
safdar-azeem <safdarazeem00@gmail.com>
2022-09-26 23:14:07 +0500

    Add ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import CustomRoute from './config/routes.config'
 import { authRoutes, privateRoutes, publicRoutes } from './routes/routes'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
 	return (
@@ -7,6 +9,7 @@ const App = () => {
 			<CustomRoute
 				routes={[...publicRoutes, ...authRoutes, ...privateRoutes]}
 			/>
+			<ToastContainer />
 		</section>
 	)
 }
