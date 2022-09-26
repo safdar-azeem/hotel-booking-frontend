51f53ef7aa5a895c5344cc2988f58149e049fd7b
safdar-azeem <safdarazeem00@gmail.com>
2022-09-26 23:13:15 +0500

    Add store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store/store'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import 'semantic-ui-css/semantic.min.css'
 import './assets/sass/main.scss'
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
