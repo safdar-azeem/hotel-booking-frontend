85eacb91181597a48441983ecc94fe987c9e20a3
safdar-azeem <safdarazeem00@gmail.com>
2022-09-26 23:13:55 +0500

    Update useForm hook

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,30 +1,68 @@
 import React, { useState } from 'react'
 import { InputOnChangeData } from 'semantic-ui-react'
-import { IForm } from '../types/forms.type'
+import { IForm, IFormState } from '../types/forms.type'
 
 const useForm = (formJson: IForm[]) => {
-	const [form, setForm] = useState({})
-
-	React.useEffect(() => {
+	const getInitialState = () => {
+		let formValues: IFormState = {}
 		formJson.forEach((formItem: IForm) => {
-			setForm({
-				...form,
-				[formItem.name]: '',
-			})
+			formValues[formItem.name] = {
+				value: formItem?.value || '',
+				error: '',
+			}
 		})
-	}, [formJson])
+		return formValues
+	}
+
+	const [form, setForm] = useState<IFormState>(getInitialState)
+	const [isValid, setIsValid] = useState<boolean>(false)
 
 	const handleChange = (
 		event: React.ChangeEvent<HTMLInputElement>,
 		data: InputOnChangeData
 	): void => {
+		const formItem = formJson.find((item) => item.name === data.name)
 		setForm({
 			...form,
-			[data.name]: data.value,
+			[data.name]: {
+				...form[data.name],
+				value: data.value,
+				error: data.value === '' && formItem?.ErrorMessage,
+			},
 		})
 	}
 
-	return { form, handleChange }
+	const doValidate = () => {
+		let errors: IFormState = {}
+		formJson.forEach((formItem: IForm) => {
+			if (formItem.required && form && !form[formItem.name].value) {
+				errors = {
+					...errors,
+					[formItem.name]: {
+						value: form[formItem.name].value,
+						error: form[formItem.name].value
+							? ''
+							: formItem.ErrorMessage,
+					},
+				}
+			}
+		})
+		setForm({ ...form, ...errors })
+		setIsValid(Object.keys(errors).length === 0)
+	}
+
+	const setFormError = (name: string, error?: string) => {
+		const formItem = formJson.find((item) => item.name === name)
+		const formItemError = error || formItem?.ErrorMessage || ''
+		const newForm: IFormState = { ...form }
+		newForm[name] = {
+			...newForm[name],
+			error: formItemError,
+		}
+		setForm(newForm)
+	}
+
+	return { form, handleChange, doValidate, isValid, setFormError }
 }
 
 export default useForm
