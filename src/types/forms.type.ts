4f09f28bda79e423d034497949f3d7822df850dc
safdar-azeem <safdarazeem00@gmail.com>
2022-09-26 23:12:14 +0500

    Add interface for IformState

--- src/types/forms.type.ts
+++ src/types/forms.type.ts
@@ -6,4 +6,12 @@ export interface IForm {
 	placeholder: string
 	ErrorMessage: string
 	required: boolean
+	value?: string
+}
+
+export interface IFormState {
+	[key: string]: {
+		value: any
+		error: string
+	}
 }
