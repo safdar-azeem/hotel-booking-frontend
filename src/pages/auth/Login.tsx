775d6056800d09fc02a06014ecfc961f9e885c6c
safdar-azeem <safdarazeem00@gmail.com>
2022-09-26 23:13:04 +0500

    Refactor signup login form

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,16 +1,42 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button, Checkbox, Form, Header } from 'semantic-ui-react'
 import '../../assets/sass/main.scss'
 import useForm from '../../hooks/useForm'
 import loginJson from '../../json/login.json'
 import { IForm } from '../../types/forms.type'
 import { Routes } from '../../types/routes.type'
+import { useLoginMutation } from '../../store/reducers/auth.reducer'
+import Cookies from 'js-cookie'
+import { toast } from 'react-toastify'
+import React from 'react'
 
 const Login = () => {
-	const { form, handleChange } = useForm(loginJson)
+	const navigation = useNavigate()
+	const { form, handleChange, doValidate, isValid } = useForm(loginJson)
+	const [login, { data, error }] = useLoginMutation()
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (!isValid) return doValidate()
+		login({
+			email: form.email.value,
+			password: form.password.value,
+		})
+	}
+
+	React.useEffect(() => {
+		error && 'data' in error && toast.error(error.data.message)
+		if (data) {
+			Cookies.set('token', data.token)
+			toast.success('Login successful')
+			navigation(Routes.Home)
+		}
+	}, [error, data])
 
 	return (
-		<Form className='p-10 mx-auto mt-20'>
+		<Form
+			className='p-10 mx-auto mt-20'
+			onSubmit={handleSubmit}>
 			<Header
 				as='h2'
 				textAlign='center'>
@@ -21,18 +47,19 @@ const Login = () => {
 					<Form.Field>
 						<Form.Input
 							error={
-								false && {
-									content: inputField.ErrorMessage,
-									pointing: 'below',
-								}
+								form[inputField.name]?.error
+									? { content: form[inputField.name].error }
+									: false
 							}
 							fluid
 							size='large'
 							onChange={handleChange}
 							type={inputField.type}
-							label={inputField.label}
+							label={`${inputField.label}${
+								inputField.required ? '*' : ''
+							}`}
 							name={inputField.name}
-							value={form[inputField.name as keyof typeof form]}
+							value={form[inputField.name].value}
 							placeholder={inputField.placeholder}
 							id={inputField.id}
 						/>
