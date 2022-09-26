d73d5c39b440485e8091477f01137aeeb8697579
safdar-azeem <safdarazeem00@gmail.com>
2022-09-26 23:12:56 +0500

    Refactor signup form

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,16 +1,55 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import { Button, Checkbox, Form, Header, Image } from 'semantic-ui-react'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import {
+	Button,
+	Checkbox,
+	Form,
+	Header,
+	Image,
+	Loader,
+} from 'semantic-ui-react'
 import '../../assets/sass/main.scss'
 import { Routes } from '../../types/routes.type'
 import signupJson from '../../json/signup.json'
 import { IForm } from '../../types/forms.type'
 import useForm from '../../hooks/useForm'
+import { useSignupMutation } from '../../store/reducers/auth.reducer'
+import Cookies from 'js-cookie'
+import { toast } from 'react-toastify'
 
 const SignUp = () => {
-	const { form, handleChange } = useForm(signupJson)
+	const navigation = useNavigate()
+	const [signup, { data, error, isLoading }] = useSignupMutation()
+	const { form, handleChange, doValidate, setFormError, isValid } =
+		useForm(signupJson)
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (!isValid) return doValidate()
+		if (form.password.value !== form.confirmPassword.value) {
+			return setFormError('confirmPassword', 'Passwords do not match')
+		}
+
+		signup({
+			email: form.email.value,
+			password: form.password.value,
+			name: form.name.value,
+		})
+	}
+
+	React.useEffect(() => {
+		error && 'data' in error && toast.error(error.data.message)
+		if (data) {
+			Cookies.set('token', data.token)
+			toast.success('Signup successful')
+			navigation(Routes.Login)
+		}
+	}, [error, data])
+
 	return (
-		<Form className='p-10 mx-auto mt-20'>
+		<Form
+			className='p-10 mx-auto mt-20'
+			onSubmit={handleSubmit}>
 			<Header
 				as='h2'
 				textAlign='center'>
@@ -21,17 +60,19 @@ const SignUp = () => {
 					<Form.Field>
 						<Form.Input
 							error={
-								false && {
-									content: inputField.ErrorMessage,
-									pointing: 'below',
-								}
+								form[inputField.name]?.error
+									? { content: form[inputField.name].error }
+									: false
 							}
 							fluid
 							size='large'
 							type={inputField.type}
 							onChange={handleChange}
-							label={inputField.label}
-							value={form[inputField.name as keyof typeof form]}
+							name={inputField.name}
+							label={`${inputField.label}${
+								inputField.required ? '*' : ''
+							}`}
+							value={form[inputField.name].value}
 							placeholder={inputField.placeholder}
 							id={inputField.id}
 						/>
@@ -43,6 +84,8 @@ const SignUp = () => {
 				type='submit'
 				color='violet'
 				size='large'
+				disabled={isLoading}
+				loading={isLoading}
 				fluid>
 				Login
 			</Button>
